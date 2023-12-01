b138176b3bcff29d0cb42cf09a2603bf0d7a5e87
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-01 17:33:24 -0500

    feat: Add a general framework of files to start from

---
+++ app/controllers/station_controller.rb
@@ -0,0 +1,13 @@
+class StationController < ApplicationController
+  def index
+    # [ ] done
+
+    # User Story 1, Parent Index 
+    
+    # For each parent table
+    # As a visitor
+    # When I visit '/parents'
+    # Then I see the name of each parent record in the system
+  end
+end
+
---
+++ app/models/trains.rb
@@ -0,0 +1,3 @@
+class Trains < ApplicationRecord
+  
+end
