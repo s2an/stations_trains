7e2594e1fd057710ea86aa4a2649f64efb4c7ded
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-05 17:51:15 -0500

    refactor: Move tests from stations_spec to station_trains_spec

--- spec/features/stations_spec.rb
+++ spec/features/stations_spec.rb
@@ -35,26 +35,6 @@ RSpec.describe Station, type: :feature do
     expect(page).to have_content(station.platform_count)
     expect(page).to have_content(station.food_stand ? "Yes" : "No" )
   end
-  
-  it 'I1,US5: A visitor sees each child, with attributes, associated with the parent, by id' do
-    # User Story 5, Parent Children Index [ √ ] done
-    # As a visitor
-    # When I visit '/parents/:parent_id/child_table_name'
-    # Then I see each Child that is associated with that Parent with each Child's attributes
-    # (data from each column that is on the child table)
-    station = Station.create!(name: "Roanoke Station", platform_count: 1, food_stand: false)
-    train1 = Train.create!(train_type: "Two-Rail", capacity: 442, is_express: false, station: station)
-    train2 = Train.create!(train_type: "Monorail", capacity: 88, is_express: true, station: station)
-    
-    visit "/stations/#{station.id}/trains"
-
-    expect(page).to have_content(train1.train_type)
-    expect(page).to have_content(train1.capacity)
-    expect(page).to have_content(train1.is_express ? "Yes" : "No" )
-    expect(page).to have_content(train2.train_type)
-    expect(page).to have_content(train2.capacity)
-    expect(page).to have_content(train2.is_express ? "Yes" : "No" )
-  end
 
   it 'I1,US6: A visitor sees the parents, with thier attributes, sorted by recently created' do
     # User Story 6 Parent Index sorted by Most Recently Created [ √ ] done
@@ -70,20 +50,5 @@ RSpec.describe Station, type: :feature do
     expect(page).to have_content(station1.created_at)
     expect(page).to have_content(station2.created_at)
   end
-
-  it 'I1,US7: A visitor sees one parent and counts their associated children' do
-    # User Story 7 Parent Child Count [ √ ] done
-    # As a visitor
-    # When I visit a parent's show page
-    # I see a count of the number of children associated with this parent
-    station = Station.create!(name: "Roanoke Station", platform_count: 1, food_stand: false)
-    Train.create!(train_type: "Two-Rail", capacity: 442, is_express: false, station: station)
-    Train.create!(train_type: "Monorail", capacity: 88, is_express: true, station: station)
-    
-    visit "/stations/#{station.id}"
-
-    expect(page).to have_content(station.trains.count)
-  end
-
 
 end
